c09cc3f44612e009ec43570c0375beea5cc758c4
ussamasaeed <ussamasaeed5@gmail.com>
2026-08-18 17:28:25 +0500

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Path, Response
 import json
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Annotated, Optional
 
 # Load data funcation
 def load_data():
@@ -38,15 +39,15 @@ def read_health():
 #     data = load_data()
 #     return data
 
-@app.get("/tasks/{id}")
-def task(id: int):
-    data = load_data()
+# @app.get("/tasks/{id}")
+# def task(id: int):
+#     data = load_data()
 
-    for task in data["tasks"]:
-        if task["id"] == id:
-            return task
+#     for task in data["tasks"]:
+#         if task["id"] == id:
+#             return task
 
-        else : raise HTTPException(status_code=404, detail="Task not found")
+#         else : raise HTTPException(status_code=404, detail="Task not found")
 
 # Stage 3 start
 # create pydantic class
@@ -77,3 +78,49 @@ def create_task(task: task_create):
 
     save_data(data)
 
+# stage 4 start
+
+# pydantic class
+class task_update(BaseModel):
+    title: Annotated[Optional[str], Field(default=None)]
+    done: Annotated[Optional[str], Field(default=None)]
+
+# endpoint for update
+@app.put("/task/{id}")
+def update_task(id, update: task_update):
+
+    data = load_data()
+    update_data_only = update.model_dump(exclude_unset=True)
+
+    for task in data["tasks"]:
+        
+        if str(task["id"]) == str(id): 
+            
+            for key, value in update_data_only.items():
+                task[key] = value
+
+            save_data(data)
+            return {"message": "Task updated successfully", "task": task}
+
+    raise HTTPException(status_code=404, detail="id not found")
+    
+# Delete task endpoint
+@app.delete("/tasks/{id}", status_code=204)
+def delete_task(id: int):
+    data = load_data()
+
+    for i, task in enumerate(data["tasks"]):
+        if task["id"] == id:
+
+            # Remove task
+            data["tasks"].pop(i)
+
+            save_data(data)
+
+            # 204 = no response body
+            return Response(status_code=204)
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
