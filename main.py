268199c7a1f742537c9f1cea4099f4ee12912d72
ussamasaeed <ussamasaeed5@gmail.com>
2026-09-22 16:16:28 +0500

    Stage 4: explored SQLite

--- main.py
+++ main.py
@@ -217,3 +217,10 @@ def delete_task(id: int):
 
     # 204 means no response body
     return Response(status_code=204)
+
+
+# SELECT * FROM tasks;
+# SELECT * FROM tasks WHERE done = 1;
+# SELECT COUNT(*) FROM tasks;
+# UPDATE tasks SET done = 1;
+# DELETE FROM tasks WHERE done = 1;
